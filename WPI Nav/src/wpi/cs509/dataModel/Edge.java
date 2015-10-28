f20d5b183d0f28e70f6a961682eeb7a883deee08
randallhong <randallhong@users.noreply.github.com>
2015-10-27 20:09:58 -0400

    Update Edge.java
    The first vision of "edge" class

--- WPI Nav/src/wpi/cs509/dataModel/Edge.java
+++ WPI Nav/src/wpi/cs509/dataModel/Edge.java
@@ -1,5 +1,43 @@
 package wpi.cs509.dataModel;
-
+/**
+ * This is the class to describe the edge between a pair of points.
+ * @author Zhiming
+ *@version 1.0
+ */
 public class Edge {
-
+  private int id;
+	private int weight;
+	private String description;
+	private int sPointId;
+	private int ePointId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getsPointId() {
+		return sPointId;
+	}
+	public void setsPointId(int sPointId) {
+		this.sPointId = sPointId;
+	}
+	public int getePointId() {
+		return ePointId;
+	}
+	public void setePointId(int ePointId) {
+		this.ePointId = ePointId;
+	}
 }
