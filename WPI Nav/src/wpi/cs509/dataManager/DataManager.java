9795158c4c0ba7f669c6b2949fcb4cb34f10afdb
Sunjiawei58 <sunjiawei5548833@163.com>
2015-10-31 15:14:34 -0400

    1st commit

--- WPI Nav/src/wpi/cs509/dataManager/DataManager.java
+++ WPI Nav/src/wpi/cs509/dataManager/DataManager.java
@@ -1,5 +1,21 @@
 package wpi.cs509.dataManager;
 
-public class DataManager {
+import wpi.cs509.dataModel.*;
 
+public class DataManager {
+	public static boolean addPoint(Point p,File f){
+		
+	}
+	public static boolean addEdge(Edge e,File f){
+		
+	}
+	public static Graph getGraph(Graph g,File f){
+		
+	}
+	public static getListOfMaps(){
+		
+	}
+	public static boolean addMap(){
+		
+	}
 }
