55281c9d7ffb38200bfa3cd5826a809604fd39a7
randallhong <randallhong@users.noreply.github.com>
2015-11-05 13:53:47 -0500

    Update Graph.java

--- WPI Nav/src/wpi/cs509/dataModel/Graph.java
+++ WPI Nav/src/wpi/cs509/dataModel/Graph.java
@@ -1,5 +1,19 @@
 package wpi.cs509.dataModel;
 
 public class Graph {
+  private ArrayList<Point> points;
+	private ArrayList<Edge> edges;
+	public ArrayList<Point> getPoints() {
+		return points;
+	}
+	public void setPoints(ArrayList<Point> points) {
+		this.points = points;
+	}
+	public ArrayList<Edge> getEdges() {
+		return edges;
+	}
+	public void setEdges(ArrayList<Edge> edges) {
+		this.edges = edges;
+	}
 
 }
