13900ec3121e0fb06f36cecbbc6dd91469ca741e
Sunjiawei58 <sunjiawei5548833@163.com>
2015-11-05 20:47:08 -0500

    done api1

--- WPI Nav/src/wpi/cs509/dataManager/DataManager.java
+++ WPI Nav/src/wpi/cs509/dataManager/DataManager.java
@@ -1,25 +1,137 @@
 package wpi.cs509.dataManager;
 
-import wpi.cs509.dataModel.*;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import wpi.cs509.dataModel.Edge;
+import wpi.cs509.dataModel.Graph;
+import wpi.cs509.dataModel.Map;
+import wpi.cs509.dataModel.Point;
 public class DataManager {
-	public static boolean addPoint(Point p,String fileName){
-		return false;
+	public static boolean addPoint(String buildingName,String floorName, int x, int y, boolean isEntrance,boolean isLocation,String name ){
 		
+		Connection conn = null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		int success=0;
+		int floorNum=88;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="insert into points (x,y,buildingName,floorNum,isEntrance,attribute,name)values(?,?,?,?,?,?,?)";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+			switch(floorName)
+			{
+			case "SubBasement":
+				floorNum = -1;break;
+			case "Basement":
+				floorNum = 0;break;
+			case "First Floor":
+				floorNum = 1;break;
+			case "Second Floor":
+				floorNum = 2;break;
+			case "Third Floor":
+				floorNum = 3;break;
+			case "Fourth Floor":
+				floorNum = 4;break;
+			case "Fifth Floor":
+				floorNum = 5;break;
+				default:
+					floorNum=88;break;
+			}
+			
+			ps1.setInt(1, x);
+			ps1.setInt(2, y);
+			ps1.setString(3, buildingName);
+			ps1.setInt(4, floorNum);
+			ps1.setBoolean(5, isEntrance);
+			if(isLocation)
+			{
+				ps1.setString(6, "Location");
+			}
+			else ps1.setString(6, "PassageWay");
+			ps1.setString(7, name);
+			
+			
+			
+
+			
+			
+			success  = ps1.executeUpdate();
+			
+			ps1.close();
+			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(success!=0)
+		{
+			return true;
+		}
+		else return false;
+	
+
 	}
-	public static boolean addEdge(Edge e,String fileName){
-		return false;
+	public static boolean addEdge(int point1ID,int point2ID,float weight){
 		
+		
+		Connection conn=null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		int success=0;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="insert into edge (point1id,point2id,weight)values(?,?,?)";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+						
+			ps1.setInt(1, point1ID);
+			ps1.setInt(2, point2ID);
+			ps1.setFloat(3, weight);
+
+			
+			
+			success  = ps1.executeUpdate();
+			
+			ps1.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		if(success!=0)
+		{
+			return true;
+		}
+		else return false;
+		
+
 	}
 	public static Graph getGraph(Graph g,String fileName){
+
 		
 		FileReader fr = null;
 		try {
@@ -32,9 +144,13 @@ public class DataManager {
 		BufferedReader br = new BufferedReader(fr);
 		
 		String valueString = null;
+		
+		
 		try {
 			while((valueString=br.readLine())!=null)
 			{
+				
+				Point p = new Point();
 				if(valueString.equals("MapName"))
 				{
 					System.out.println(br.readLine());
@@ -69,9 +185,16 @@ public class DataManager {
 			if(resultsPoint.length==7)   // the complete information of points.
 			{
 				System.out.println("ID:"+resultsPoint[0]);
+				p.setId(Integer.parseInt(resultsPoint[0]));
+				
 				System.out.println("X:"+resultsPoint[1]);
+				p.setX(Integer.parseInt(resultsPoint[1]));
+				
 				System.out.println("Y:"+resultsPoint[2]);
+				p.setY(Integer.parseInt(resultsPoint[2]));
+				
 				System.out.println("BuildingName:"+resultsPoint[3]);
+				
 				System.out.println("FloorNumber:"+resultsPoint[4]);
 				System.out.println("EntranceID:"+resultsPoint[5]);
 				System.out.println("PointAttribute:"+resultsPoint[6]);
@@ -120,13 +243,20 @@ public class DataManager {
 		
 		
 	}
-	public static Graph getGraphByNameWithDB(String fileName)
+	
+	public static Graph getGraphByNameWithDB(String buildingName)
 	{
 		Graph graph = new Graph();
+		ArrayList<Point> pointsArray= new ArrayList<Point>();
+		ArrayList<Edge> edgesArray= new ArrayList<Edge>();
+		
+		//for points and edges buffer.
 		Connection conn =null;
-		String sql="";
+		String sqlPoint="";
+		String sqlEdge="";
 		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
-		Point p = new Point();
+		//Point p = new Point();
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("successfully load the driver");
@@ -134,13 +264,19 @@ public class DataManager {
 			
 			conn = DriverManager.getConnection(url);
 			
-			sql = "select * from points";
-			PreparedStatement ps = conn.prepareStatement(sql);
+			sqlPoint = "select * from points where buildingName like ?";
+			PreparedStatement ps1 = conn.prepareStatement(sqlPoint);
 			
-			ResultSet resultPoints = ps.executeQuery();
+			ps1.setString(1, buildingName+"%");
+			sqlEdge = "select * from edge where point1id = ?";
+			PreparedStatement ps2 = conn.prepareStatement(sqlEdge);
+			
+			
+			ResultSet resultPoints = ps1.executeQuery();
+			ResultSet resultEdges;
 			while(resultPoints.next())
 			{
-				
+				Point p = new Point();
 //				resultPoints.getString(0);
 				p.setId(resultPoints.getInt(1));
 				p.setX(resultPoints.getInt(2));
@@ -156,23 +292,33 @@ public class DataManager {
 				String attribute = resultPoints.getString(7);
 				p.setName(resultPoints.getString(8));
 				System.out.println("the id is .."+resultPoints.getString(1));
+				pointsArray.add(p);
+				
+				ps2.setInt(1, resultPoints.getInt(1));
+				
+				resultEdges = ps2.executeQuery();
+				while(resultEdges.next())
+				{
+					Edge edge = new Edge();
+					edge.setId(resultEdges.getInt(1));
+					edge.setsPointId(resultEdges.getInt(2));
+					edge.setePointId(resultEdges.getInt(3));
+					edge.setWeight(resultEdges.getFloat(4));
+					System.out.println(edge);
+					edgesArray.add(edge);
+				}
+				 resultEdges.close();
+				
 			}
 			
 			
 			
 		    resultPoints.close(); 
-		    ps.close();
+		   
+		    ps1.close();
+		    ps2.close();
 		    
-		    
-		    sql="select * from edges";
-		    ps=conn.prepareStatement(sql);
-		    ResultSet resultEdges = ps.executeQuery();
-		    
-		    while(resultEdges.next())
-		    {
-		    	
-		    }
-		    
+		   
 		    
 		    
 		    conn.close(); 
@@ -184,18 +330,398 @@ public class DataManager {
 			e.printStackTrace();
 		}
 		
+		graph.setPoints(pointsArray);
+		graph.setEdges(edgesArray);
+		
+		//System.out.println(pointsArray.get(0).getId());
+		//System.out.println(pointsArray.get(0).getId());
+		//System.out.println("points size is "+graph.getPoints().get(0).getId());  //testing
+		//System.out.println("points size is "+graph.getPoints().get(1).getId());  //testing
+		//System.out.println("weight is "+graph.getEdges().get(0).getWeight()); //testing
 		return graph;
 	}
-	public static void getListOfMaps(){
+	
+	public static ArrayList<String> getFloorsMapsByBuildingName(String buildingName)
+	{
+		ArrayList<String> floorName = new ArrayList<String>();
+		
+		Connection conn =null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("successfully load the driver");
+			
+			conn = DriverManager.getConnection(url);
+			
+			sql="select distinct floorNum from points where buildingName like ? ";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+			
+			ps1.setString(1, buildingName+"%");
+			ResultSet rs = ps1.executeQuery();
+			while(rs.next())
+			{
+				int floorNum =0;
+				String floor="";
+				floorNum = rs.getInt(1);
+				switch(floorNum)
+				{
+				case -1:
+					floor="SubBasement";break;
+				case 0:
+					floor = "Basement";break;
+				case 1:
+					floor = "First Floor";break;
+				case 2:
+					floor = "Second Floor";break;
+				case 3:
+					floor = "Third Floor";break;
+				case 4:
+					floor="Fourth Floor";break;
+				case 5:
+					floor="Fifth Floor";break;
+				default:
+						floor="";
+				}
+				
+				floorName.add(floor);
+				
+			}
+			rs.close();
+			ps1.close();
+			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return floorName;
+	}
+	
+	public static ArrayList<String> getBuildings(){
+		
+		ArrayList<String> buildingsName = new ArrayList<String>();
+		
+		Connection conn =null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="select distinct name from points where buildingName = ? ";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+			
+			ps1.setString(1,"Campus");
+			ResultSet rs = ps1.executeQuery();
+			while(rs.next())
+			{
+				String building="";
+				building = rs.getString(1);
+				
+				buildingsName.add(building);
+
+				
+			}
+			rs.close();
+			ps1.close();
+			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return buildingsName;
 		
 	}
-	public static void addMap(){
+
+	
+	public static ArrayList<Point> getLocationsByMapID(String buildingName,String floorName)
+	{
+		ArrayList<Point> locations = new ArrayList<Point>();
+		int floorNum=0;
+		Connection conn =null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="select * from points where buildingName=? and floorNum=? and attribute<>'PassageWay' ";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+			
+			ps1.setString(1,buildingName);
+			switch(floorName)
+			{
+			case "SubBasement":
+				floorNum = -1;break;
+			case "Basement":
+				floorNum = 0;break;
+			case "First Floor":
+				floorNum = 1;break;
+			case "Second Floor":
+				floorNum = 2;break;
+			case "Third Floor":
+				floorNum = 3;break;
+			case "Fourth Floor":
+				floorNum = 4;break;
+			case "Fifth Floor":
+				floorNum = 5;break;
+				default:
+					floorNum=88;break;
+			}
+			ps1.setInt(2, floorNum);
+			ResultSet rs = ps1.executeQuery();
+			while(rs.next())
+			{
+				Point p = new Point();
+//				resultPoints.getString(0);
+				p.setId(rs.getInt(1));
+				p.setX(rs.getInt(2));
+				p.setY(rs.getInt(3));
+				p.setBuildingName(rs.getString(4));
+				p.setFloorNum(rs.getInt(5));
+				int isEntrance = rs.getInt(6);
+				if(isEntrance!=0)
+				{
+					p.setMapEntrance(true);
+				}
+				else p.setMapEntrance(false);
+				String attribute = rs.getString(7);
+				if(attribute!="PassageWay")
+				{
+					p.setDestination(true);
+				}
+				
+				p.setName(rs.getString(8));
+				System.out.println("the id is .."+rs.getString(1));
+				locations.add(p);
+				
+				
+			}
+			rs.close();
+			ps1.close();
+			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return locations;
+	}
+
+	public static boolean saveMap(String name, String path,float scale)
+	{
+		Connection conn =null;
+		int success=0;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="insert into map (scale,name,path)values(?,?,?)";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+			
+			
+			ps1.setFloat(1, scale);
+			ps1.setString(2, name);
+			ps1.setString(3, path);
+			
+			
+			success  = ps1.executeUpdate();
+			
+			ps1.close();
+			conn.close();
+			
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(success!=0)
+		return true;
+		else return false;
+	}
+	
+	public static ArrayList<Map> getAllMaps()
+	{
+		ArrayList<Map> mapList = new ArrayList<Map>();
+		
+		Connection conn = null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="select * from map";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+				
+			ResultSet resultMap = ps1.executeQuery();
+			while(resultMap.next())
+			{
+				Map map = new Map();
+				map.setId(resultMap.getInt(1));
+				map.setScale(resultMap.getFloat(2));
+				map.setName(resultMap.getString(3));
+				map.setFileLocation(resultMap.getString(4));
+				mapList.add(map);
+			}
+			resultMap.close();
+			ps1.close();
+			conn.close();
+			
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(mapList.size());
+		
+		return mapList;
+	}
+	
+	public static ArrayList<Point>getAllPoints()
+	{
+		ArrayList<Point> pointsArray = new ArrayList<Point>();
+		
+		Connection conn = null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="select * from points";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+				
+			ResultSet resultPoints = ps1.executeQuery();
+			while(resultPoints.next())
+			{
+				Point p = new Point();
+//				resultPoints.getString(0);
+				p.setId(resultPoints.getInt(1));
+				p.setX(resultPoints.getInt(2));
+				p.setY(resultPoints.getInt(3));
+				p.setBuildingName(resultPoints.getString(4));
+				p.setFloorNum(resultPoints.getInt(5));
+				int isEntrance = resultPoints.getInt(6);
+				if(isEntrance!=0)
+				{
+					p.setMapEntrance(true);
+				}
+				else p.setMapEntrance(false);
+				String attribute = resultPoints.getString(7);
+				p.setName(resultPoints.getString(8));
+				System.out.println("the id is .."+resultPoints.getString(1));
+				pointsArray.add(p);
+			}
+			resultPoints.close();
+			ps1.close();
+			conn.close();
+			
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(pointsArray.size());
+		
+		return pointsArray;
+	}
+	public static ArrayList<Edge>getAllEdges()
+	{
+		ArrayList<Edge> edgesArray = new ArrayList<Edge>();
+		
+		Connection conn = null;
+		String sql="";
+		String url = "jdbc:mysql://localhost:3306/wpinavi?"+"user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url);
+			
+			sql="select * from edge";
+			PreparedStatement ps1 = conn.prepareStatement(sql);
+				
+			ResultSet resultEdges = ps1.executeQuery();
+			while(resultEdges.next())
+			{
+				Edge edge = new Edge();
+				edge.setId(resultEdges.getInt(1));
+				edge.setsPointId(resultEdges.getInt(2));
+				edge.setePointId(resultEdges.getInt(3));
+				edge.setWeight(resultEdges.getFloat(4));
+				System.out.println(edge);
+				edgesArray.add(edge);
+			}
+			 resultEdges.close();
+			ps1.close();
+			conn.close();
+			
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(edgesArray.size());
+		
+		return edgesArray;
 		
 	}
 	public static void main(String[] args){
 		Graph graph= new Graph();
 //		getGraph(graph,"src/HF1.txt");
-		getGraphByNameWithDB("src/HF1/txt");
-	}
+		getGraphByNameWithDB("Higgins");
+		float testScale = 15.5f;
+		float testWeight = 3.2f;
+		//getFloorsMapsByBuildingName("Higgins");
+		//System.out.println(getFloorsMapsByBuildingName("Fuller").get(0));
+		//System.out.println(getFloorsMapsByBuildingName("Fuller").get(1));
+		//System.out.println(getBuildings().get(0));
+		//System.out.println(getBuildings().get(1));
+		//System.out.println(getLocationsByMapID("HigginsLab","First Floor").get(0).getX());
+		//System.out.println(saveMap("HigginsLab","c://testForIMG",testScale));
+		//System.out.println(addPoint("FullerLab","Third Floor", 600, 480, false,true,"Room333"));
+		//System.out.println(addEdge(1,2,testWeight));
+		System.out.println(getAllMaps());
+		System.out.println(getAllPoints());
+		System.out.println(getAllEdges());
 	}
 }
