6d56239c3096ed5497cebc5e0491413ec95d2628
crystal <xsun3@wpi.edu>
2015-11-29 15:51:08 -0500

    update RouteLine to 3.0f

---
+++ WPI Nav/src/wpi/cs509/ui/components/RouteLine.java
@@ -0,0 +1,48 @@
+package wpi.cs509.ui.components;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+
+import javax.swing.JPanel;
+
+public class RouteLine extends JPanel {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 879309659307886465L;
+	
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private Color color;
+
+	public RouteLine(Color color, int x1, int y1, int x2, int y2) {
+		super();
+		
+		// Custom Setting
+		this.color = color;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.setBounds(0, 0, 1024, 730);
+		this.setBackground(new Color(Color.TRANSLUCENT));
+		this.setOpaque(false);
+		
+	}
+	
+	@Override public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D)g;
+		Stroke stroke = new BasicStroke(3.0f);
+		g2d.setStroke(stroke);
+		g.setColor(this.color);
+		g.drawLine(x1, y1, x2, y2);
+		
+    }
+}
--- WPI Nav/src/wpi/cs509/ui/util/Util.java
+++ WPI Nav/src/wpi/cs509/ui/util/Util.java
@@ -12,6 +12,7 @@ import wpi.cs509.dataModel.Edge;
 import wpi.cs509.dataModel.Point;
 import wpi.cs509.ui.components.ImagePanel;
 import wpi.cs509.ui.components.Line;
+import wpi.cs509.ui.components.RouteLine;
 import wpi.cs509.ui.components.SolidPoint;
 
 public class Util {
@@ -27,7 +28,7 @@ public class Util {
 			Point p1 =P.get(i);
 			Point p2 =P.get(i+1);
 //			Line path = new Line(Color.decode("#929292"), p1.getX(), p1.getY(), p2.getX(), p2.getY());
-			Line path = new Line(Color.decode("#D55E00"), p1.getX(), p1.getY(), p2.getX(), p2.getY());
+			RouteLine path = new RouteLine(Color.decode("#D55E00"), p1.getX(), p1.getY(), p2.getX(), p2.getY());
 			a.add(path, new Integer(1), 0);
 			a.repaint();
 			
